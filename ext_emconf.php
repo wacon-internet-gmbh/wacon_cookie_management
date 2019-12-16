a65ad40f035a6963ed8263b9c1331dc251341879
Kerstin Schmitt <kerstin.schmitt@wacon.de>
2019-12-16 14:48:48 +0100

    Beschreibung aktualisiert

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.3-9.5.99',
+            'typo3' => '7.6.32-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
