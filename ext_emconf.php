5bb88975912935c66d053378ffd7816453b2a53d
Kerstin Schmitt <kerstin.schmitt@wacon.de>
2020-02-12 08:43:33 +0100

    Add Description

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,15 +12,16 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Wacon Cookie Management',
-    'description' => 'Verschiedene Cookies können individuell zugelassen werden. Unterscheidung in notwendige und optionale Cookies',
+    'description' => 'WCM is a typo3 extension that enables website visitors to control and manage the usage of cookies on the website(commonly known as "cookie consent box"). Thus it helps website owner to be in line with the rules of the General Data Protection Regulation (GDPR) which took effect on 25 May 2018.',
     'category' => 'plugin',
     'author' => 'Kerstin Schmitt',
     'author_email' => 'info@wacon.de',
+    'author_company' => 'WACON Internet GmbH',
     'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.4',
+    'version' => '1.1.5',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.31-9.5.99',
