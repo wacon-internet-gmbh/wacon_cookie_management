30c60cdf22bad02bb40a8ea28dbeb710c6718d0c
Kerstin Schmitt <kerstin.schmitt@wacon.de>
2021-02-17 12:31:52 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,7 +13,7 @@ call_user_func(
             ],
             // non-cacheable actions
             [
-                'Cookie' => ''
+                'Cookie' => 'list'
             ]
         );
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
