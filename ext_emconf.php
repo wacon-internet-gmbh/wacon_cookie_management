e3c6ac98f8605b59dd73bfc4c7b96301a0edd759
Kerstin Schmitt <kerstin.schmitt@wacon.de>
2021-10-05 12:48:54 +0200

    Change TYPO3-Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,10 +16,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.2.0',
+    'version' => '3.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.3.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
