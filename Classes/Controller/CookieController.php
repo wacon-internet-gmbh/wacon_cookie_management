8ab6d29f5e1cb870aa00f61195466649928847b0
Philipp Kuhlmay <philipp.kuhlmay@wacon.de>
2023-06-09 15:27:37 +0200

    [BUGFIX] ensure $cookie is set to a string instead of null

--- Classes/Controller/CookieController.php
+++ Classes/Controller/CookieController.php
@@ -204,8 +204,8 @@ class CookieController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
      */
     public function showAction()
     {
-      $cookie = $_COOKIE['waconcookiemanagement'] ?? null;; 
-      $content2 = $this->settings['bild'] ?? null;
+      $cookie = $_COOKIE['waconcookiemanagement'] ?? '';
+      $content2 = $this->settings['bild'] ?? '';
       
       $showcookie = $this->settings['cookie'];
       $nocookiecontentarray = null;
